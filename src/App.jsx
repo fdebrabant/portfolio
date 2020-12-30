f0f788eafe78fe1f7a9ae8a2b813537f170aeb73
fdebrabant <debrabant.fanny@gmail.com>
2020-12-30 17:57:59 +0100

    add fonts, title style

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../src/css/main.css';
+import '../src/css/title.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Fanny Debrabant</h1>
+    </div>
+  );
+}
+
+export default App;
