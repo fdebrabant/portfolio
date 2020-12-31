5a75dfcb58102d136047b54581169e3ee505d239
fdebrabant <debrabant.fanny@gmail.com>
2020-12-31 16:20:53 +0100

    update css banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,42 +4,45 @@ import bannerLeft from '../assets/img/banner-Left-mob.svg';
 import bannerRight from '../assets/img/banner-Right-mob.svg';
 
 const HeadBanner = styled.div`
-  display: flex;
-  justify-content: space-between;
+  position: relative;
 `;
 
 const BannerL = styled.div`
+  position: absolute;
+  left: 0;
   height: 350px;
-  width: 290px;
+  width: 160px;
   background: url(${bannerLeft}) no-repeat center/cover;
-  z-index: 100;
   @media (min-width: 450px) {
     height: 520px;
+    width: 296px;
   }
   @media (min-width: 830px) {
-    height: 640px;
+    height: 701px;
     width: 400px;
   }
   @media (min-width: 1150px) {
-    height: 820px;
+    height: 895px;
     width: 510px;
   }
 `;
 
 const BannerR = styled.div`
   height: 350px;
-  width: 290px;
+  width: 160px;
+  position: absolute;
+  right: 0;
   background: url(${bannerRight}) no-repeat center/cover;
-  z-index: 100;
   @media (min-width: 450px) {
     height: 520px;
+    width: 296px;
   }
   @media (min-width: 830px) {
-    height: 640px;
+    height: 701px;
     width: 400px;
   }
   @media (min-width: 1150px) {
-    height: 820px;
+    height: 895px;
     width: 510px;
   }
 `;
