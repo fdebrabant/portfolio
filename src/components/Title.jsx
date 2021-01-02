36d2de8dd3f1d32bd53abf92e80f9253f3293c81
fdebrabant <debrabant.fanny@gmail.com>
2021-01-02 14:30:17 +0100

    add proptypes title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import '../css/title.css';
 import triangle from '../assets/img/triangle-title.svg';
+import PropTypes from 'prop-types';
 
 const OutTitle = styled.div`
    background: url(${triangle}) no-repeat center/cover; 
@@ -12,10 +13,18 @@ const OutTitle = styled.div`
 `;
 
 export default function Title(props) {
+    const { color, text, outline } = props;
+
     return (
         <div>
-        <h1 className={props.color}>{props.text}</h1>
-        <OutTitle className={props.outline}/>      
+        <h1 className={color}>{text}</h1>
+        <OutTitle className={outline}/>      
         </div>
     )
 }
+
+Title.propTypes = {
+    color: PropTypes.string,
+    outline: PropTypes.string,
+    text: PropTypes.string
+};
