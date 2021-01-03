3dc35d5ab6e65ed8e7c6a02fc7db391400a0f321
fdebrabant <debrabant.fanny@gmail.com>
2021-01-03 19:38:25 +0100

    modify status

--- src/views/Status.jsx
+++ src/views/Status.jsx
@@ -5,8 +5,13 @@ import Text from '../components/Text';
 export default function Status() {
     return (
         <div>
-            <TitleSecond text='En recherche de stage du 15 fevrier au 15 juin 2021' color='blue' />
-            <Text />
+            <TitleSecond text='En recherche de stage du 15 fevrier au 15 juin 2021' size='secondTitle' />
+            <Text 
+                text='Forte d’une experience passée dans 
+                la sphère de l’experiementation agricole, 
+                je suis aujourd’hui en reconversion dans 
+                le Web developpement.'
+            />
         </div>
     )
 }
